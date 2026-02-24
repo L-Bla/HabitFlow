86841004a6e91d05fd1c7a0a2c0b8343399490b1
L-Bla <lblaga00@fesb.hr>
2026-02-24 23:52:38 +0100

    Fix draw param2 none

--- mens-sana/src/actions/createChart.ts
+++ mens-sana/src/actions/createChart.ts
@@ -155,7 +155,7 @@ async function genTitle(user_id: string, param1: string, param2: string | null){
       ))
       title += part[0].name
   }
-  if (param2){
+  if (param2 != "none"){
     title += " & "
     if (param2==="energy" || param2==="pleasantness"){
       title += param2
